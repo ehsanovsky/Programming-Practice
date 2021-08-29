4c55b079b17f93519eaa8ee82d8984be1487516a
Ehsan <ehsansafari.p@gmail.com>
2021-08-30 01:15:25 +0430

    function name is

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
+
+// object
+let numbers =[5,9,1];
+console.log(numbers[1]);
+numbers[1]=15;
+console.log(numbers[1]);
+let person= {
+name : "nader" , 
+age : 30,
+gender : "male" ,
+addres : {
+    countery : "Iran",
+    city : "Tehran",
+    mahal : {
+            zone : 22 ,
+            esm : ["chitgar","naziabad"] 
+        }
+    },
+Hobbies : ["movies ","codding"]
+}
+person.email = "nader@email.com"
+console.log(person.addres.mahal.esm[1]);
+
+
 // function
+function sayHello(name){
+    console.log("Hello World , my name is " + name);
+}
+sayHello("ehsan");
