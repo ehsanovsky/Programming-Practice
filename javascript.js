807567fb3f7d47d61a5dd90c9142048294f867b6
Ehsan <ehsansafari.p@gmail.com>
2021-08-29 17:03:43 +0430

    jsobjects

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+let numbers =[5,9,1];
+console.log(numbers[1]);
+numbers[1]=15;
+console.log(numbers[1]);
+let person= {
+name : "nader" , 
+age : 30,
+gender : "male" ,
+    addres : {
+        countery : "Iran",
+        city : "Tehran",
+    },
+    Hobbies : ["movies ","codding"]
+}
+console.log(person.addres.city);
