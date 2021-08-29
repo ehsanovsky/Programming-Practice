ad31979e456cef7786f625dc5a64ab8e66b1bdd3
Ehsan <ehsansafari.p@gmail.com>
2021-08-30 02:13:00 +0430

    dk

--- javascript.js
+++ javascript.js
@@ -1,29 +1,48 @@
 
 // object
-let numbers =[5,9,1];
-console.log(numbers[1]);
-numbers[1]=15;
-console.log(numbers[1]);
-let person= {
-name : "nader" , 
-age : 30,
-gender : "male" ,
-addres : {
-    countery : "Iran",
-    city : "Tehran",
-    mahal : {
-            zone : 22 ,
-            esm : ["chitgar","naziabad"] 
-        }
-    },
-Hobbies : ["movies ","codding"]
-}
-person.email = "nader@email.com"
-console.log(person.addres.mahal.esm[1]);
+// let numbers =[5,9,1];
+// console.log(numbers[1]);
+// numbers[1]=15;
+// console.log(numbers[1]);
+// let person= {
+// name : "nader" , 
+// age : 30,
+// gender : "male" ,
+// addres : {
+//     countery : "Iran",
+//     city : "Tehran",
+//     mahal : {
+//             zone : 22 ,
+//             esm : ["chitgar","naziabad"] 
+//         }
+//     },
+// Hobbies : ["movies ","codding"]
+// }
+// person.email = "nader@email.com"
+// console.log(person.addres.mahal.esm[1]);
 
 
 // function
-function sayHello(name){
-    console.log("Hello World , my name is " + name);
-}
-sayHello("ehsan");
+// function sayHello(name , age ){
+//     console.log("Hello World , my name is " + name +" and i'm " + age +" years old") ;
+// }
+// sayHello("ehsan" , 26);
+// sayHello("zahra" , 24);
+
+// function square (number){
+//    squared = number * number ;
+//    console.log(squared);
+// }
+// square (9);
+
+// heap and stack //
+
+let name =  "mahdi";
+let aName ;
+ 
+aName = name ;
+console.log(name);
+console.log(aName);
+
+aName = "bahram";
+console.log(aName);
